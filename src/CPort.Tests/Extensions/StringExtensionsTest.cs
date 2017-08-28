2bf19c3454e9dd7f17ffe1772bb3ce6b53068694
Yanos <ygrenier@ygrenier.com>
2017-08-28 22:32:25 +0200

    Add tests on the extensions.

---
+++ src/CPort.Tests/Extensions/StringExtensionsTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CPort.Tests.Extensions
+{
+    public class StringExtensionsTest
+    {
+        [Fact]
+        public void ContainsChar()
+        {
+            string line = "AbC";
+
+            Assert.True(line.Contains('A'));
+            Assert.True(line.Contains('b'));
+            Assert.False(line.Contains('c'));
+            Assert.True(line.Contains('C'));
+
+            line = null;
+
+            Assert.False(line.Contains('A'));
+            Assert.False(line.Contains('b'));
+            Assert.False(line.Contains('c'));
+            Assert.False(line.Contains('C'));
+        }
+    }
+}
---
+++ src/CPort.Tests/Extensions/TypeExtensionsTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CPort.Tests.Extensions
+{
+    public class TypeExtensionsTest
+    {
+
+        [Fact]
+        public void GetTypeCode()
+        {
+            Type tp = null;
+            Assert.Equal(TypeCode.Empty, tp.GetTypeCode());
+            Assert.Equal(TypeCode.Boolean, typeof(bool).GetTypeCode());
+            Assert.Equal(TypeCode.Byte, typeof(byte).GetTypeCode());
+            Assert.Equal(TypeCode.Char, typeof(char).GetTypeCode());
+            Assert.Equal(TypeCode.UInt16, typeof(ushort).GetTypeCode());
+            Assert.Equal(TypeCode.UInt32, typeof(uint).GetTypeCode());
+            Assert.Equal(TypeCode.UInt64, typeof(ulong).GetTypeCode());
+            Assert.Equal(TypeCode.SByte, typeof(sbyte).GetTypeCode());
+            Assert.Equal(TypeCode.Int16, typeof(short).GetTypeCode());
+            Assert.Equal(TypeCode.Int32, typeof(int).GetTypeCode());
+            Assert.Equal(TypeCode.Int64, typeof(long).GetTypeCode());
+            Assert.Equal(TypeCode.String, typeof(string).GetTypeCode());
+            Assert.Equal(TypeCode.Single, typeof(float).GetTypeCode());
+            Assert.Equal(TypeCode.Double, typeof(double).GetTypeCode());
+            Assert.Equal(TypeCode.DateTime, typeof(DateTime).GetTypeCode());
+            Assert.Equal(TypeCode.Decimal, typeof(Decimal).GetTypeCode());
+            Assert.Equal(TypeCode.Object, typeof(TypeExtensionsTest).GetTypeCode());
+            Assert.Equal(TypeCode.Object, typeof(TypeCode).GetTypeCode());
+        }
+
+    }
+}
