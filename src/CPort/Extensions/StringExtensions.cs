38726e2ea72e880541a2262b8097fd7935e6d4d1
Yanos <ygrenier@ygrenier.com>
2017-08-28 21:49:43 +0200

    Implementation of the 'scanf' function. Implementation of the tests for 'scanf'.

---
+++ src/CPort/Extensions/StringExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CPort
+{
+    /// <summary>
+    /// Extensions of string
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Check if a string contains a char
+        /// </summary>
+        public static bool Contains(this string s, char c)
+        {
+            return s?.IndexOf(c) >= 0;
+        }
+    }
+}
