8a3afa8c957808dc59f12214cda88c7f3b7aad29
Yanos <ygrenier@ygrenier.com>
2017-08-28 21:27:30 +0200

    Reorganize source folder. Prepare the base of the project.

--- CPort/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace CPort
-{
-    public class Class1
-    {
-    }
-}
---
+++ src/CPort/C.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CPort
+{
+    /// <summary>
+    /// C port static class
+    /// </summary>
+    public static class C
+    {
+    }
+}
