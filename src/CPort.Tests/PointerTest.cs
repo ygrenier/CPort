79d02a17761e4ea3a33570d0f31ae83b56fa17ab
Yanos <ygrenier@ygrenier.com>
2017-08-28 22:47:13 +0200

    Create a basic C pointer simulator. Tests.

---
+++ src/CPort.Tests/PointerTest.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace CPort.Tests
+{
+    public class PointerTest
+    {
+        [Fact]
+        public void Create()
+        {
+            Pointer<int> p = new Pointer<int>();
+            Assert.True(p.IsNull);
+            Assert.Null(p.Source);
+            Assert.Equal(0, p.Index);
+
+            p = new Pointer<int>(10);
+            Assert.False(p.IsNull);
+            Assert.NotNull(p.Source);
+            Assert.Equal(10, p.Source.Count);
+            Assert.Equal(0, p.Index);
+
+            int[] source = new int[] { 1, 2, 3 };
+            p = new Pointer<int>(source);
+            Assert.False(p.IsNull);
+            Assert.NotNull(p.Source);
+            Assert.Same(source, p.Source);
+            Assert.Equal(0, p.Index);
+
+            p = new Pointer<int>(source, 4);
+            Assert.False(p.IsNull);
+            Assert.NotNull(p.Source);
+            Assert.Same(source, p.Source);
+            Assert.Equal(4, p.Index);
+
+            p = new Pointer<int>(source, -4);
+            Assert.False(p.IsNull);
+            Assert.NotNull(p.Source);
+            Assert.Same(source, p.Source);
+            Assert.Equal(0, p.Index);
+
+            p = new Pointer<int>(null);
+            Assert.True(p.IsNull);
+            Assert.Null(p.Source);
+            Assert.Equal(0, p.Index);
+
+            p = new Pointer<int>(null, 4);
+            Assert.True(p.IsNull);
+            Assert.Null(p.Source);
+            Assert.Equal(0, p.Index);
+        }
+
+        [Fact]
+        public void AsEnumerable()
+        {
+            int[] source = Enumerable.Range(1, 10).ToArray();
+
+            var p = new Pointer<int>(source);
+            Assert.Equal(source, p.ToArray());
+
+            p = new Pointer<int>(source, 6);
+            Assert.Equal(new int[] { 7, 8, 9, 10 }, p.ToArray());
+
+            p = new Pointer<int>(source, 50);
+            Assert.Equal(new int[] { }, p.ToArray());
+
+            p = new Pointer<int>(source, -50);
+            Assert.Equal(source, p.ToArray());
+
+            IEnumerable penum = p;
+            Assert.Equal(source, penum.OfType<int>().ToArray());
+
+            p = new Pointer<int>(null, 0);
+            Assert.Equal(new int[] { }, p.ToArray());
+        }
+
+    }
+}
---
+++ src/CPort/Pointer.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CPort
+{
+
+    /// <summary>
+    /// C pointer simulator
+    /// </summary>
+    public struct Pointer<T> : IEnumerable<T>
+    {
+
+        #region Ctors
+        /// <summary>
+        /// Create a new pointer
+        /// </summary>
+        public Pointer(IList<T> source) : this(source, 0)
+        { }
+
+        /// <summary>
+        /// Create a new pointer with a source and an index
+        /// </summary>
+        public Pointer(IList<T> source, int index)
+        {
+            Source = source;
+            Index = Math.Max(0, source != null ? index : 0);
+        }
+
+        /// <summary>
+        /// Create a new pointer based on a new buffer of a <paramref name="size"/> elements
+        /// </summary>
+        public Pointer(int size)
+        {
+            Source = new T[size];
+            Index = 0;
+        }
+        #endregion
+
+        #region Enumerable
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            return (Source ?? Enumerable.Empty<T>()).Skip(Index).GetEnumerator();
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable<T>)this).GetEnumerator();
+        }
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Indicates if the pointer is null
+        /// </summary>
+        public bool IsNull => Source == null;
+
+        /// <summary>
+        /// Source
+        /// </summary>
+        public IList<T> Source { get; private set; }
+
+        /// <summary>
+        /// Current index in the source of the pointer
+        /// </summary>
+        public int Index { get; private set; }
+
+        #endregion
+    }
+
+}
