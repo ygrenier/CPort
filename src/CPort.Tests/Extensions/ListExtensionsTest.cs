06767848f89101b19abc7deab3362b1f42d41a87
Yanos <ygrenier@ygrenier.com>
2017-08-29 21:21:17 +0200

    Add method to create pointer from a list. Tests.

---
+++ src/CPort.Tests/Extensions/ListExtensionsTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CPort.Tests.Extensions
+{
+    public class ListExtensionsTest
+    {
+        [Fact]
+        public void GetPointer()
+        {
+            int[] source = new int[] { 1, 3, 5, 7 };
+            var p = source.GetPointer();
+            Assert.False(p.IsNull);
+            Assert.Equal(4, p.Source.Count);
+            Assert.Equal(1, p[0]);
+            Assert.Equal(3, p[1]);
+            Assert.Equal(5, p[2]);
+            Assert.Equal(7, p[3]);
+
+            source = new int[] { };
+            p = source.GetPointer();
+            Assert.False(p.IsNull);
+            Assert.Equal(0, p.Source.Count);
+
+            source = null;
+            p = source.GetPointer();
+            Assert.True(p.IsNull);
+            Assert.Null(p.Source);
+        }
+    }
+}
---
+++ src/CPort/Extensions/ListExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CPort
+{
+    /// <summary>
+    /// Extensions of list
+    /// </summary>
+    public static class ListExtensions
+    {
+        /// <summary>
+        /// Create a pointer from a list
+        /// </summary>
+        public static Pointer<T> GetPointer<T>(this IList<T> source)
+        {
+            return new Pointer<T>(source);
+        }
+    }
+}
