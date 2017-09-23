e0681de1bab330119bdeb17ce5217417dedfb80c
Yanos <ygrenier@ygrenier.com>
2017-09-23 14:54:59 +0200

    strncpy() implementation and tests.

--- src/CPort.Tests/CStringTest.cs
+++ src/CPort.Tests/CStringTest.cs
@@ -19,5 +19,23 @@ namespace CPort.Tests
             Assert.Equal(new char[] { 'T', 'e', 's', 't', '\0', '5' }, actual.Take(6));
             Assert.Equal("Test\0567890123456789\0", new string(actual.ToArray()));
         }
+
+        [Fact]
+        public void Cstrncpy()
+        {
+            var source = "Test".GetPointer();
+            var dest = "01234567890123456789".GetPointer();
+
+            var actual = strncpy(dest + 1, source, 3);
+            Assert.Equal(new char[] { '0', 'T', 'e', 's', '4', '5' }, dest.Take(6));
+            Assert.Equal(new char[] { 'T', 'e', 's', '4', '5', '6' }, actual.Take(6));
+            Assert.Equal("0Tes4567890123456789\0", new string(dest.ToArray()));
+            Assert.Equal("Tes4567890123456789\0", new string(actual.ToArray()));
+
+            actual = strncpy(dest, source, 7);
+            Assert.Equal(new char[] { 'T', 'e', 's', 't', '\0', '\0', '\0', '7' }, actual.Take(8));
+            Assert.Equal("Test\0\0\07890123456789\0", new string(actual.ToArray()));
+
+        }
     }
 }
--- src/CPort/C.string.cs
+++ src/CPort/C.string.cs
@@ -28,5 +28,31 @@ namespace CPort
             } while (c != '\0');
             return s;
         }
+        /// <summary>
+        /// strncpy()
+        /// </summary>
+        /// <param name="s">Destination string</param>
+        /// <param name="ct">Source string</param>
+        /// <param name="n">Count of characters to copy</param>
+        /// <returns>returns <paramref name="s"/></returns>
+        public static Pointer<char> strncpy(Pointer<char> s, Pointer<char> ct, int n)
+        {
+            var sd = s;
+            var ss = ct;
+            int cnt = n;
+            char c;
+            do
+            {
+                c = ss++;
+                sd.Value = c;
+                sd++;
+            } while (--n > 0 && c != '\0');
+            while (n-- > 0)
+            {
+                sd.Value = '\0';
+                sd++;
+            }
+            return s;
+        }
     }
 }
