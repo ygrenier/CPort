09ab1c262d485c5f7a1b95fc9a676cb2235f6ebd
Yanos <ygrenier@ygrenier.com>
2017-09-23 14:24:47 +0200

    strcpy() implementation and tests.

---
+++ src/CPort.Tests/CStringTest.cs
@@ -0,0 +1,23 @@
+﻿using static CPort.C;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using System.Linq;
+
+namespace CPort.Tests
+{
+    public class CStringTest
+    {
+        [Fact]
+        public void Cstrcpy()
+        {
+            var source = "Test".GetPointer();
+            var dest = "01234567890123456789".GetPointer();
+
+            var actual = strcpy(dest, source);
+            Assert.Equal(new char[] { 'T', 'e', 's', 't', '\0', '5' }, actual.Take(6));
+            Assert.Equal("Test\0567890123456789\0", new string(actual.ToArray()));
+        }
+    }
+}
---
+++ src/CPort/C.string.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CPort
+{
+    /// <summary>
+    /// string.h
+    /// </summary>
+    static partial class C
+    {
+        /// <summary>
+        /// strcpy()
+        /// </summary>
+        /// <param name="s">Destination string</param>
+        /// <param name="ct">Source string</param>
+        /// <returns>returns <paramref name="s"/></returns>
+        public static Pointer<char> strcpy(Pointer<char> s, Pointer<char> ct)
+        {
+            var sd = s;
+            var ss = ct;
+            char c;
+            do
+            {
+                c = ss++;
+                sd.Value = c;
+                sd++;
+            } while (c != '\0');
+            return s;
+        }
+    }
+}
