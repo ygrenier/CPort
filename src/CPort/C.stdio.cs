9d618d503a2bd1b8875f218d0e4038e0b9888529
Yanos <ygrenier@ygrenier.com>
2017-09-25 19:57:59 +0200

    Prepare stdio.h functions.

---
+++ src/CPort/C.stdio.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CPort
+{
+#pragma warning disable IDE1006
+    /// <summary>
+    /// stdio.h functions
+    /// </summary>
+    static partial class C
+    {
+
+    }
+#pragma warning restore IDE1006
+}
---
+++ src/CPort/FILE.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CPort
+{
+
+    /// <summary>
+    /// C file object
+    /// </summary>
+    public class FILE
+    {
+
+    }
+
+}
