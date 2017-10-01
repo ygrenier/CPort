35e0b6d4d395b64f2e9895ffbee245843fe1f63f
Yanos <ygrenier@ygrenier.com>
2017-10-01 10:39:09 +0200

    Create a PChar for pointer of char management and better string conversion.

---
+++ src/CPort.Tests/PCharTest.cs
@@ -0,0 +1,336 @@
+﻿using static CPort.C;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace CPort.Tests
+{
+    public class PCharTest
+    {
+
+        [Fact]
+        public void Create()
+        {
+            PChar p = new PChar();
+            Assert.True(p.IsNull);
+            Assert.Null(p.Source);
+            Assert.Equal(0, p.Index);
+
+            p = new PChar(10);
+            Assert.False(p.IsNull);
+            Assert.NotNull(p.Source);
+            Assert.Equal(10, p.Source.Count);
+            Assert.Equal(0, p.Index);
+
+            char[] source = new char[] { '1', '2', '3' };
+            p = new PChar(source);
+            Assert.False(p.IsNull);
+            Assert.NotNull(p.Source);
+            Assert.Same(source, p.Source);
+            Assert.Equal(0, p.Index);
+
+            p = new PChar(source, 4);
+            Assert.False(p.IsNull);
+            Assert.NotNull(p.Source);
+            Assert.Same(source, p.Source);
+            Assert.Equal(4, p.Index);
+
+            p = new PChar(source, -4);
+            Assert.False(p.IsNull);
+            Assert.NotNull(p.Source);
+            Assert.Same(source, p.Source);
+            Assert.Equal(0, p.Index);
+
+            p = new PChar((char[])null);
+            Assert.True(p.IsNull);
+            Assert.Null(p.Source);
+            Assert.Equal(0, p.Index);
+
+            p = new PChar(null, 4);
+            Assert.True(p.IsNull);
+            Assert.Null(p.Source);
+            Assert.Equal(0, p.Index);
+
+            p = new PChar("abcd");
+            Assert.False(p.IsNull);
+            Assert.NotNull(p.Source);
+            Assert.Equal(5, p.Source.Count);
+            Assert.Equal(new char[] { 'a', 'b', 'c', 'd', '\0' }, p.Source);
+            Assert.Equal(0, p.Index);
+
+            p = new PChar((string)null);
+            Assert.True(p.IsNull);
+            Assert.Null(p.Source);
+            Assert.Equal(0, p.Index);
+        }
+
+        [Fact]
+        public void AsEnumerable()
+        {
+            char[] source = Enumerable.Range(0, 10).Select(i => (char)('a' + i)).ToArray();
+
+            var p = new PChar(source);
+            Assert.Equal(source, p.ToArray());
+
+            p = new PChar(source, 6);
+            Assert.Equal(new char[] { 'g', 'h', 'i', 'j' }, p.ToArray());
+
+            p = new PChar(source, 50);
+            Assert.Equal(new char[] { }, p.ToArray());
+
+            p = new PChar(source, -50);
+            Assert.Equal(source, p.ToArray());
+
+            IEnumerable penum = p;
+            Assert.Equal(source, penum.OfType<char>().ToArray());
+
+            p = new PChar(null, 0);
+            Assert.Equal(new char[] { }, p.ToArray());
+        }
+
+        [Fact]
+        public void TestGetHashCode()
+        {
+            char[] source = Enumerable.Range(0, 10).Select(i => (char)('a' + i)).ToArray();
+
+            var p = new PChar(source);
+            Assert.Equal(source.GetHashCode() ^ 0, p.GetHashCode());
+
+            p = new PChar(source, 6);
+            Assert.Equal(source.GetHashCode() ^ 6, p.GetHashCode());
+
+            p = new PChar(source, 50);
+            Assert.Equal(source.GetHashCode() ^ 50, p.GetHashCode());
+
+            p = new PChar(source, -50);
+            Assert.Equal(source.GetHashCode() ^ 0, p.GetHashCode());
+
+            p = new PChar(null, 0);
+            Assert.Equal(0, p.GetHashCode());
+        }
+
+        [Fact]
+        public void TestEquals()
+        {
+            char[] source = Enumerable.Range(0, 10).Select(i => (char)('a' + i)).ToArray();
+
+            var p1 = new PChar(source);
+            Assert.True(p1.Equals(source));
+
+            var p2 = new PChar(source, 6);
+            Assert.False(p2.Equals(source));
+
+            var p3 = new PChar(null, 0);
+            Assert.False(p3.Equals(source));
+
+            Assert.True(p1.Equals(new PChar(source, 0)));
+            Assert.False(p1.Equals(new PChar(source, 6)));
+            Assert.False(p2.Equals(new PChar(source, 0)));
+            Assert.True(p2.Equals(new PChar(source, 6)));
+            Assert.False(p3.Equals(new PChar(source, 0)));
+            Assert.False(p3.Equals(new PChar(source, 6)));
+
+            Assert.True(p1.Equals(new Pointer<char>(source, 0)));
+            Assert.False(p1.Equals(new Pointer<char>(source, 6)));
+            Assert.False(p2.Equals(new Pointer<char>(source, 0)));
+            Assert.True(p2.Equals(new Pointer<char>(source, 6)));
+            Assert.False(p3.Equals(new Pointer<char>(source, 0)));
+            Assert.False(p3.Equals(new Pointer<char>(source, 6)));
+
+            Assert.False(p1.Equals(null));
+            Assert.False(p1.Equals(123));
+        }
+
+        [Fact]
+        public void GetString()
+        {
+            var pointer = new PChar(new char[] { 'T', 'e', 's', 't', '\0', 'T', 'e', 's', 't', '\0' });
+            Assert.Equal("Test", pointer.GetString());
+
+            Assert.Equal("st", (pointer + 2).GetString());
+            Assert.Equal("", (pointer + 4).GetString());
+            Assert.Equal("est", (pointer + 6).GetString());
+            Assert.Equal("t", (pointer + 8).GetString());
+            Assert.Equal("", (pointer + 10).GetString());
+
+            pointer = (char[])null;
+            Assert.Equal(null, pointer.GetString());
+            Assert.Equal(null, (pointer + 2).GetString());
+
+        }
+
+        [Fact]
+        public void Casts()
+        {
+            char[] array = Enumerable.Range(0, 10).Select(i => (char)('a' + i)).ToArray();
+            List<char> list = new List<char>(array);
+
+            PChar p = array;
+            Assert.Same(array, p.Source);
+            Assert.Equal(0, p.Index);
+
+            p = list;
+            Assert.Same(list, p.Source);
+            Assert.Equal(0, p.Index);
+
+            char[] narray = (char[])p;
+            Assert.Equal(narray, array);
+
+            List<char> nlist = (List<char>)p;
+            Assert.Equal(nlist, list);
+
+            p = new PChar(array, 5);
+            narray = (char[])p;
+            Assert.Equal(narray, array.Skip(5));
+            nlist = (List<char>)p;
+            Assert.Equal(nlist, list.Skip(5));
+
+            p = new PChar();
+            narray = (char[])p;
+            Assert.Null(narray);
+            nlist = (List<char>)p;
+            Assert.Null(nlist);
+
+            // Cast from Pointer<char>
+            p = array;
+            Pointer<char> poc = p;
+            Assert.Same(array, poc.Source);
+            Assert.Equal(0, poc.Index);
+            p = p + 3;
+            poc = p;
+            Assert.Same(array, poc.Source);
+            Assert.Equal(3, poc.Index);
+            poc --;
+            p = poc;
+            Assert.Same(array, p.Source);
+            Assert.Equal(2, p.Index);
+
+            // Cast of string
+            p = "test";
+            Assert.Equal(new char[] { 't', 'e', 's', 't', '\0' }, p.Source);
+            Assert.Equal(0, p.Index);
+            p[2] = 'Z';
+            string s = (string)p;
+            Assert.Equal("teZt", s);
+
+            // Cast from NullPointer
+            p = array;
+            Assert.False(p.IsNull);
+            p = NULL;
+            Assert.True(p.IsNull);
+        }
+
+        [Fact]
+        public void ValueAccess()
+        {
+            char[] source = Enumerable.Range(0, 10).Select(i => (char)('a' + i)).ToArray();
+
+            var p = new PChar(source, 5);
+            Assert.Equal('f', p);
+            Assert.Equal('f', p[0]);
+            Assert.Equal('f', p.Value);
+            Assert.Throws<PointerOutOfRangeException>(() => Assert.Equal('f', p[123]));
+            Assert.True(p.TryGetValue(0, out char actual));
+            Assert.Equal('f', actual);
+            Assert.False(p.TryGetValue(123, out actual));
+            Assert.Equal(0, actual);
+
+            p.Value = 'm';
+            p[2] = 'n';
+            Assert.Throws<PointerOutOfRangeException>(() => p[123] = 'm');
+            Assert.True(p.TrySetValue('s', 0));
+            Assert.False(p.TrySetValue('z', 123));
+
+            Assert.Equal(new char[] { 'a', 'b', 'c', 'd', 'e', 's', 'g', 'n', 'i', 'j' }, source);
+
+            p = new PChar(null, 5);
+            Assert.Throws<PointerNullException>(() => actual = p);
+            Assert.Throws<PointerNullException>(() => actual = p.Value);
+            Assert.Throws<PointerNullException>(() => actual = p[0]);
+            Assert.Throws<PointerNullException>(() => p.Value = 'm');
+            Assert.Throws<PointerNullException>(() => p[0] = 'n');
+            Assert.False(p.TryGetValue(0, out actual));
+            Assert.False(p.TrySetValue('s', 0));
+
+        }
+
+        [Fact]
+        public void PointerAddSub()
+        {
+            char[] source = Enumerable.Range(0, 10).Select(i => (char)('a' + i)).ToArray();
+
+            var p1 = new PChar(source);
+            Assert.Equal(0, p1.Index);
+            var p2 = p1 + 4;
+            Assert.Equal(0, p1.Index);
+            Assert.Equal(4, p2.Index);
+            var p3 = p2 - 6;
+            Assert.Equal(0, p1.Index);
+            Assert.Equal(4, p2.Index);
+            Assert.Equal(0, p3.Index);
+        }
+
+        [Fact]
+        public void PointerIncDec()
+        {
+            char[] source = Enumerable.Range(0, 10).Select(i => (char)('a' + i)).ToArray();
+
+            var p = new PChar(source);
+            Assert.Equal(0, p.Index);
+            Assert.Equal(0, (p++).Index);
+            Assert.Equal(1, p.Index);
+            Assert.Equal(2, (++p).Index);
+            Assert.Equal(2, p.Index);
+            Assert.Equal(2, (p--).Index);
+            Assert.Equal(1, p.Index);
+            Assert.Equal(0, (--p).Index);
+            Assert.Equal(0, p.Index);
+            Assert.Equal(0, (--p).Index);
+            Assert.Equal(0, p.Index);
+            Assert.Equal(0, (--p).Index);
+            Assert.Equal(0, p.Index);
+        }
+
+        [Fact]
+        public void Equality()
+        {
+            char[] source = Enumerable.Range(0, 10).Select(i => (char)('a' + i)).ToArray();
+
+            var p1 = new PChar(source);
+            Assert.True(p1 == source);
+            Assert.False(p1 != source);
+            Assert.True(source == p1);
+            Assert.False(source != p1);
+
+            var p2 = new PChar(source, 6);
+            Assert.False(p2 == source);
+            Assert.True(p2 != source);
+
+            var p3 = new PChar(null, 0);
+            Assert.False(p3 == source);
+            Assert.True(p3 != source);
+
+            Assert.True(p1 == new PChar(source, 0));
+            Assert.False(p1 != new PChar(source, 0));
+            Assert.False(p1 == new PChar(source, 6));
+            Assert.True(p1 != new PChar(source, 6));
+            Assert.False(p2 == new PChar(source, 0));
+            Assert.True(p2 != new PChar(source, 0));
+            Assert.True(p2 == new PChar(source, 6));
+            Assert.False(p2 != new PChar(source, 6));
+            Assert.False(p3 == new PChar(source, 0));
+            Assert.True(p3 != new PChar(source, 0));
+            Assert.False(p3 == new PChar(source, 6));
+            Assert.True(p3 != new PChar(source, 6));
+
+            Assert.False(p1 == null);
+            Assert.True(p1 != null);
+            Assert.True(p3 == null);
+            Assert.False(p3 != null);
+        }
+
+    }
+}
---
+++ src/CPort/PChar.cs
@@ -0,0 +1,383 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CPort
+{
+    /// <summary>
+    /// C pointer of char simulator
+    /// </summary>
+    public struct PChar : IPointer, IEnumerable<char>
+    {
+
+        #region Ctors
+        /// <summary>
+        /// Create a new pointer
+        /// </summary>
+        public PChar(IList<char> source) : this(source, 0)
+        { }
+
+        /// <summary>
+        /// Create a new pointer with a source and an index
+        /// </summary>
+        public PChar(IList<char> source, int index)
+        {
+            Source = source;
+            Index = Math.Max(0, source != null ? index : 0);
+        }
+
+        /// <summary>
+        /// Create a new pointer based on a new buffer of a <paramref name="size"/> elements
+        /// </summary>
+        public PChar(int size)
+        {
+            Source = new char[size];
+            Index = 0;
+        }
+
+        /// <summary>
+        /// Create a new pointer from a .Net string
+        /// </summary>
+        public PChar(string s)
+        {
+            Index = 0;
+            if (s == null)
+            {
+                Source = null;
+            }
+            else
+            {
+                var arr = new char[s.Length + 1];   // Add a '\0'
+                s.CopyTo(0, arr, 0, s.Length);
+                arr[arr.Length - 1] = '\0';
+                Source = arr;
+            }
+        }
+        #endregion
+
+        #region Enumerable
+        IEnumerator<char> IEnumerable<char>.GetEnumerator()
+        {
+            return (Source ?? Enumerable.Empty<char>()).Skip(Index).GetEnumerator();
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable<char>)this).GetEnumerator();
+        }
+        #endregion
+
+        #region Overrides
+
+        /// <summary>
+        /// HashCode
+        /// </summary>
+        public override int GetHashCode()
+        {
+            return (Source?.GetHashCode() ?? 0) ^ Index.GetHashCode();
+        }
+
+        /// <summary>
+        /// Determine if <paramref name="obj"/> is equals with this pointer
+        /// </summary>
+        public override bool Equals(object obj)
+        {
+            if (obj is IList<char>)
+            {
+                return Source == obj && Index == 0;
+            }
+            else if (obj is PChar pch)
+            {
+                return Source == pch.Source && Index == pch.Index;
+            }
+            else if (obj is Pointer<char> pobj)
+            {
+                return Source == pobj.Source && Index == pobj.Index;
+            }
+            return base.Equals(obj);
+        }
+
+        #endregion
+
+        #region String management
+
+        /// <summary>
+        /// Extract the string
+        /// </summary>
+        public string GetString()
+        {
+            if (IsNull) return null;
+            return new string(Source.Skip(Index).TakeWhile(c => c != '\0').ToArray());
+        }
+
+        #endregion
+
+        #region Casts
+
+        /// <summary>
+        /// Implicit conversion of a pointer to a pointer of char
+        /// </summary>
+        public static implicit operator PChar(Pointer<char> source)
+        {
+            return source.IsNull ? new PChar() : new PChar(source.Source, source.Index);
+        }
+
+        /// <summary>
+        /// Implicit conversion of a pointer of char to a pointer
+        /// </summary>
+        public static implicit operator Pointer<char>(PChar source)
+        {
+            return source.IsNull ? new Pointer<char>() : new Pointer<char>(source.Source, source.Index);
+        }
+
+        /// <summary>
+        /// Implicit conversion of a list to a pointer
+        /// </summary>
+        public static implicit operator PChar(List<char> source)
+        {
+            return new PChar(source);
+        }
+
+        /// <summary>
+        /// Implicit conversion of an array to a pointer
+        /// </summary>
+        public static implicit operator PChar(char[] source)
+        {
+            return new PChar(source);
+        }
+
+        /// <summary>
+        /// Implicit conversion from a null pointer
+        /// </summary>
+        public static implicit operator PChar(NullPointer source)
+        {
+            return new PChar();
+        }
+
+        /// <summary>
+        /// Explicit conversion of a pointer to an array
+        /// </summary>
+        public static explicit operator char[] (PChar pointer)
+        {
+            return pointer.IsNull ? null : pointer.ToArray();
+        }
+
+        /// <summary>
+        /// Explicit conversion of a pointer to a list
+        /// </summary>
+        public static explicit operator List<char>(PChar pointer)
+        {
+            return pointer.IsNull ? null : pointer.ToList();
+        }
+
+        /// <summary>
+        /// Implicit conversion of a string to a pointer
+        /// </summary>
+        public static implicit operator PChar(string s)
+        {
+            return new PChar(s);
+        }
+
+        /// <summary>
+        /// Explicit conversion of a pointer to a string
+        /// </summary>
+        public static explicit operator string(PChar pointer)
+        {
+            return pointer.GetString();
+        }
+        #endregion
+
+        #region Pointer manipulation
+
+        /// <summary>
+        /// Increment the pointer
+        /// </summary>
+        public static PChar operator +(PChar source, int offset)
+        {
+            return new PChar(source.Source, source.Index + offset);
+        }
+
+        /// <summary>
+        /// Decrement the pointer
+        /// </summary>
+        public static PChar operator -(PChar source, int offset)
+        {
+            return new PChar(source.Source, source.Index - offset);
+        }
+
+        /// <summary>
+        /// Increment the pointer
+        /// </summary>
+        public static PChar operator ++(PChar source)
+        {
+            return new PChar(source.Source, source.Index + 1);
+        }
+
+        /// <summary>
+        /// Decrement the pointer
+        /// </summary>
+        public static PChar operator --(PChar source)
+        {
+            return new PChar(source.Source, source.Index - 1);
+        }
+
+        #endregion
+
+        #region Equality
+        /// <summary>
+        /// Test the equality of the pointer with a source
+        /// </summary>
+        public static bool operator ==(PChar pointer, char[] source)
+        {
+            return pointer.Source == source && pointer.Index == 0;
+        }
+
+        /// <summary>
+        /// Test the non equality of the pointer with a source
+        /// </summary>
+        public static bool operator !=(PChar pointer, char[] source)
+        {
+            return pointer.Source != source || pointer.Index != 0;
+        }
+
+        /// <summary>
+        /// Test the equality of the pointer with a source
+        /// </summary>
+        public static bool operator ==(char[] source, PChar pointer)
+        {
+            return pointer.Source == source && pointer.Index == 0;
+        }
+
+        /// <summary>
+        /// Test the non equality of the pointer with a source
+        /// </summary>
+        public static bool operator !=(char[] source, PChar pointer)
+        {
+            return pointer.Source != source || pointer.Index != 0;
+        }
+
+        /// <summary>
+        /// Test pointers equality
+        /// </summary>
+        public static bool operator ==(PChar p1, PChar p2)
+        {
+            return p1.Source == p2.Source && p1.Index == p2.Index;
+        }
+
+        /// <summary>
+        /// Test pointers non equality
+        /// </summary>
+        public static bool operator !=(PChar p1, PChar p2)
+        {
+            return p1.Source != p2.Source || p1.Index != p2.Index;
+        }
+
+        #endregion
+
+        #region Value access
+
+        /// <summary>
+        /// Read value pointed by the pointer
+        /// </summary>
+        public static implicit operator char(PChar source)
+        {
+            return source.GetValue(0);
+        }
+
+        /// <summary>
+        /// Try to get the value at an offset of this pointer
+        /// </summary>
+        public bool TryGetValue(int offset, out char value)
+        {
+            value = default(char);
+            if (Source == null) return false;
+            int idx = Index + offset;
+            if (idx < 0 || idx >= Source.Count)
+                return false;
+            value = Source[idx];
+            return true;
+        }
+
+        /// <summary>
+        /// Try to set the value at an offset of this pointer
+        /// </summary>
+        public bool TrySetValue(char value, int offset)
+        {
+            if (Source == null) return false;
+            int idx = Index + offset;
+            if (idx < 0 || idx >= Source.Count)
+                return false;
+            Source[idx] = value;
+            return true;
+        }
+
+        /// <summary>
+        /// Get the value at an offset of this pointer
+        /// </summary>
+        /// <exception cref="PointerNullException">If the pointer is null.</exception>
+        /// <exception cref="PointerOutOfRangeException">If the real index is out of range of the source of the pointer</exception>
+        public char GetValue(int offset = 0)
+        {
+            var src = Source ?? throw new PointerNullException();
+            int idx = Index + offset;
+            if (idx < 0 || idx >= Source.Count)
+                throw new PointerOutOfRangeException(idx);
+            return Source[idx];
+        }
+
+        /// <summary>
+        /// Set the value at an offset of this pointer
+        /// </summary>
+        /// <exception cref="PointerNullException">If the pointer is null.</exception>
+        /// <exception cref="PointerOutOfRangeException">If the real index is out of range of the source of the pointer</exception>
+        public void SetValue(char value, int offset = 0)
+        {
+            var src = Source ?? throw new PointerNullException();
+            int idx = Index + offset;
+            if (idx < 0 || idx >= Source.Count)
+                throw new PointerOutOfRangeException(idx);
+            Source[idx] = value;
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Indicates if the pointer is null
+        /// </summary>
+        public bool IsNull => Source == null;
+
+        /// <summary>
+        /// Source
+        /// </summary>
+        public IList<char> Source { get; private set; }
+
+        /// <summary>
+        /// Current index in the source of the pointer
+        /// </summary>
+        public int Index { get; private set; }
+
+        /// <summary>
+        /// Get/Set value from an offset
+        /// </summary>
+        public char this[int offset]
+        {
+            get { return GetValue(offset); }
+            set { SetValue(value, offset); }
+        }
+
+        /// <summary>
+        /// Get/Set the value pointed by this pointer
+        /// </summary>
+        public char Value
+        {
+            get => GetValue(0);
+            set => SetValue(value, 0);
+        }
+
+        #endregion
+    }
+
+}
