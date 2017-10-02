9dcd2fb722632bc4e9310e011c3c2f42b906b43d
Yanos <ygrenier@ygrenier.com>
2017-10-02 19:16:31 +0200

    Create a "nul" stream.

---
+++ src/CPort.Tests/NulStreamTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CPort.Tests
+{
+    public class NulStreamTest
+    {
+        [Fact]
+        public void Tests()
+        {
+            var stream = new NulStream();
+
+            stream.Flush();
+            Assert.Equal(0, stream.Read(null, 0, 0));
+            stream.Write(null, 0, 0);
+
+            Assert.True(stream.CanRead);
+            Assert.True(stream.CanWrite);
+            Assert.False(stream.CanSeek);
+            Assert.Equal(0, stream.Position);
+            Assert.Equal(0, stream.Length);
+
+            Assert.Throws<InvalidOperationException>(() => stream.Position = 0);
+            Assert.Throws<InvalidOperationException>(() => stream.Seek(0, System.IO.SeekOrigin.Begin));
+            Assert.Throws<InvalidOperationException>(() => stream.SetLength(0));
+        }
+    }
+}
---
+++ src/CPort/NulStream.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CPort
+{
+    /// <summary>
+    /// Empty read/write stream
+    /// </summary>
+    public class NulStream : Stream
+    {
+        public override void Flush()
+        { }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        => 0;
+
+        public override long Seek(long offset, SeekOrigin origin)
+        => throw new InvalidOperationException();
+
+        public override void SetLength(long value)
+        => throw new InvalidOperationException();
+
+        public override void Write(byte[] buffer, int offset, int count)
+        { }
+
+        public override bool CanRead => true;
+
+        public override bool CanSeek => false;
+
+        public override bool CanWrite => true;
+
+        public override long Length => 0;
+
+        public override long Position { get => 0; set => throw new InvalidOperationException(); }
+    }
+
+}
